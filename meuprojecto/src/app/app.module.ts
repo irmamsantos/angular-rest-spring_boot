4fc3138a4dda7a1c0c13da0885820f2c652a120f
Irma Santos <irma.santos@gmail.com>
2019-05-20 01:10:44 +0100

    9.10. Usando two-way data binding

--- meuprojecto/src/app/app.module.ts
+++ meuprojecto/src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello/hello.component';
-import { BemVindoComponent } from './bem-vindo/bem-vindo.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HelloComponent,
-    BemVindoComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
