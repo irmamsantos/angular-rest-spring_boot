873838029c86145a478649fd11beb35154d6125f
Irma Santos <irma.santos@gmail.com>
2019-06-14 23:51:35 +0100

    16.3. Fazendo requisição com GET e recebendo o retorno

--- http/src/app/app.module.ts
+++ http/src/app/app.module.ts
@@ -1,7 +1,9 @@
+import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { CidadeService } from './cidade.service';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,9 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [CidadeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
