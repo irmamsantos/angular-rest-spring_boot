40655e589dc1bc3bd67bfa93aa58e102f98a1f5e
Irma Santos <irma.santos@gmail.com>
2019-06-16 23:13:06 +0100

    17.7. Desafio: criando a consulta e listagem de pessoas

--- algamoney-ui/src/app/pessoas/pessoas.module.ts
+++ algamoney-ui/src/app/pessoas/pessoas.module.ts
@@ -9,7 +9,6 @@ import { InputTextModule } from 'primeng/components/inputtext/inputtext';
 import { TooltipModule } from 'primeng/components/tooltip/tooltip';
 
 import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
-import { PessoasGridComponent } from './pessoas-grid/pessoas-grid.component';
 import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
 import { SharedModule } from 'app/shared/shared.module';
 
@@ -28,8 +27,7 @@ import { SharedModule } from 'app/shared/shared.module';
   ],
   declarations: [
     PessoaCadastroComponent,
-    PessoasPesquisaComponent,
-    PessoasGridComponent
+    PessoasPesquisaComponent
   ],
   exports: [
     PessoaCadastroComponent,
