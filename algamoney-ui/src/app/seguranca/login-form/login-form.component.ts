7e93abe908ced746c76aa25521bcb167c89bc74b
Irma Santos <irma.santos@gmail.com>
2019-06-23 22:27:18 +0100

    19.6. Tratando casos de erros e sucesso de autenticação

--- algamoney-ui/src/app/seguranca/login-form/login-form.component.ts
+++ algamoney-ui/src/app/seguranca/login-form/login-form.component.ts
@@ -1,20 +1,30 @@
-import { AuthService } from './../auth.service';
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+
+import { ErrorHandlerService } from './../../core/error-handler.service';
+import { AuthService } from './../auth.service';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css']
 })
-export class LoginFormComponent implements OnInit {
+export class LoginFormComponent {
 
-  constructor(private auth: AuthService) { }
-
-  ngOnInit() {
-  }
+  constructor(
+    private auth: AuthService,
+    private errorHandler: ErrorHandlerService,
+    private router: Router
+  ) { }
 
   login(usuario: string, senha: string) {
-    this.auth.login(usuario, senha);
+    this.auth.login(usuario, senha)
+      .then(() => {
+        this.router.navigate(['/lancamentos']);
+      })
+      .catch(erro => {
+        this.errorHandler.handle(erro);
+      });
   }
 
 }
